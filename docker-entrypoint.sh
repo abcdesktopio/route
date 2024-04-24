54ce654054f6ffc19e7e081e1906eea3b0d036a2
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2024-04-24 14:31:32 +0200

    add resolver directive

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-
+NAMESERVER_RESOLVER=$(grep ^nameserver /etc/resolv.conf | head -n 1 |awk '{ print $2}')
+# create nginx resolver directive
+echo "resolver $NAMESERVER_RESOLVER;">/tmp/resolv.conf
+# dump /tmp/resolv.conf
+echo NGINX resolver directive dump 
+cat /tmp/resolv.conf
+echo
 # start nginx web server
 # /usr/sbin/nginx
 echo "starting nginx web server in foreground" 
