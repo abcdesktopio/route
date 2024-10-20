42fd40360729e66805b83a7054fe39d8e4789b40
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2024-10-20 17:17:24 +0200

    fix speedtest FQDN

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,20 +11,42 @@ echo NGINX resolver directive dump
 cat /tmp/resolver.conf
 echo "resolver=$NAMESERVER_RESOLVER"
 
+#
+# root@router-od-68cf7d99b-xflnt:/etc/nginx/sites-enabled# env |grep HOST
+# MEMCACHED_SERVICE_HOST=10.98.38.64
+# HOSTNAME=router-od-68cf7d99b-xflnt
+# SPEEDTEST_SERVICE_HOST=10.103.135.215
+# PYOS_SERVICE_HOST=10.110.142.119
+# OPENLDAP_SERVICE_HOST=10.99.183.131
+# MONGODB_SERVICE_HOST=10.102.192.143
+# KUBERNETES_SERVICE_HOST=10.96.0.1
+# HTTP_ROUTER_SERVICE_HOST=10.103.230.142
+# WEBSITE_SERVICE_HOST=10.104.176.157
+# CONSOLE_SERVICE_HOST=10.111.71.209
+#
+
 while true; do
   if [ -z "$SPEEDTEST_FQDN" ]; then
-    SPEEDTEST_FQDN=$(nslookup website | awk '/^Name:/ { print $2 }')
+    # read the FQDN
+    # by default speedtest.abcdesktop.svc.cluster.local but can change by custom kubernetes cluster config
+    SPEEDTEST_FQDN=$(nslookup speedtest | awk '/^Name:/ { print $2 }')
   fi
 
   if [ -z "$PYOS_FQDN" ]; then
+    # read the FQDN
+    # by default pyos.abcdesktop.svc.cluster.local but can change by custom kubernetes cluster config
     PYOS_FQDN=$(nslookup pyos | awk '/^Name:/ { print $2 }')
   fi
 
   if [ -z "$CONSOLE_FQDN" ]; then
+    # read the FQDN
+    # by default console.abcdesktop.svc.cluster.local but can change by custom kubernetes cluster config
     CONSOLE_FQDN=$(nslookup console | awk '/^Name:/ { print $2 }')
   fi
 
   if [ -z "$WEBSITE_FQDN" ]; then
+    # read the FQDN
+    # by default website.abcdesktop.svc.cluster.local but can change by custom kubernetes cluster config
     WEBSITE_FQDN=$(nslookup website | awk '/^Name:/ { print $2 }')
   fi
   
@@ -33,7 +55,7 @@ while true; do
     echo "PYOS_FQDN=$PYOS_FQDN"
     echo "CONSOLE_FQDN=$CONSOLE_FQDN"
     echo "WEBSITE_FQDN=$WEBSITE_FQDN" 
-    echo "sleeping for 1s, for nslookup"
+    echo "sleeping for 1s, for dns resolv"
     sleep 1s
   else
     break
