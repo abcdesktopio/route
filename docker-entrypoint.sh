0577b53e02c411cb9d3297b4f9143c3210f59821
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2026-05-21 12:34:43 +0200

    Enhance comments in docker-entrypoint.sh
    Added comments for clarity on resolver configuration.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+# create file /tmp/resolver.conf
+# Read the nameservers in /etc/resolv.conf
+# the create a file for nginx with the content resolver $NAMESERVER_RESOLVER;
 if [ -z "$NAMESERVER_RESOLVER" ]; then
   NAMESERVER_RESOLVER=$(grep ^nameserver /etc/resolv.conf | head -n 1 |awk '{ print $2}')
 fi
-
 # create nginx resolver directive
 echo "resolver $NAMESERVER_RESOLVER;">/tmp/resolver.conf
-# dump /tmp/resolver.conf
+# dump the file /tmp/resolver.conf
 echo NGINX resolver directive dump 
 cat /tmp/resolver.conf
+# end of create file /tmp/resolver.conf
+
 echo "resolver=$NAMESERVER_RESOLVER"
 echo "=== dump vars ==="
 export PYOS_FQDN=${PYOS_FQDN:-${PYOS_SERVICE_HOST}}
 echo "PYOS_FQDN=$PYOS_FQDN"
 echo "PYOS_SERVICE_PORT=$PYOS_SERVICE_PORT"
-# export 
+
+# start nginx  
 exec /usr/local/openresty/nginx/sbin/nginx -p /etc/nginx -c nginx.conf -e /var/log/nginx/error.log
